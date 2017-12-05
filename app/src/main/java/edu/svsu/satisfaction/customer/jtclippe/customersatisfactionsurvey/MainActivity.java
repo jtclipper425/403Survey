1aee570fccbae3e954294181dae608afcf140c4e
jtclipper425 <jtclipper425@yahoo.com>
2017-12-04 20:04:46 -0500

    initial upload
    initial upload

---
+++ app/src/main/java/edu/svsu/satisfaction/customer/jtclippe/customersatisfactionsurvey/MainActivity.java
@@ -0,0 +1,42 @@
+package edu.svsu.satisfaction.customer.jtclippe.customersatisfactionsurvey;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView view = (TextView)findViewById(R.id.seekBarProgress);
+                view.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void showDone(View view)
+    {
+        startActivity(new Intent(this, DoneActivity.class));
+    }
+}
